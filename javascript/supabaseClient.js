130fcd6614c75cbe0e32151bcdb4edd23e06f5f0
KC-Matthew <bellanca.mathias@gmail.com>
2025-08-16 15:21:55 +0200

    Update supabaseClient.js

--- javascript/supabaseClient.js
+++ javascript/supabaseClient.js
@@ -1,7 +1,7 @@
 // javascript/supabaseClient.js
 // ⚠️ Mets tes vraies valeurs :
-const SUPABASE_URL = "https://xxxxx.supabase.co/";
-const SUPABASE_ANON_KEY = "eyJhbGciOi...";
+const SUPABASE_URL = "https://hiigdwqwtilboeimlybl.supabase.co/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpaWdkd3F3dGlsYm9laW1seWJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzODQwMTYsImV4cCI6MjA2Njk2MDAxNn0.lDjA9eZwR_EV-hkZmg2Y9WzzXlD5Zfz5u7HCs6fXYOw";
 
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
